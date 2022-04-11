f69de446ea1d10df6829eca8652a502db478d86b
DarrylA1 <darryl.avery@cloudreach.com>
2022-04-11 10:56:25 -0400

    ADD - adding in all the files for the full ASG + ELB project

--- security-groups.tf
+++ security-groups.tf
@@ -3,6 +3,29 @@ resource "aws_security_group" "app_sg" {
   description = "Creating Sg for my private app"
   vpc_id      = data.aws_vpc.main_vpc.id
 
+  #INBOUND CONNECTIONS
+  ingress {
+    description = "Allow SSH into the instance from the public instance private IP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.loadbalancer_sg.id]
+  }
+
+  #OUTBOUND CONNECTIONS
+  egress {
+    description = "Allow access to the world"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" #TCP + UDP
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "loadbalancer_sg" {
+  name        = "lb_sg"
+  description = "Allow access to this LB"
+  vpc_id      = data.aws_vpc.main_vpc.id
+
 
   #INBOUND CONNECTIONS
   ingress {
@@ -22,4 +45,3 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
